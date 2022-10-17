1fb71429e3bc6ef834091203055926abe3ce51e9
martin nkemakolam <martinnkemakolam@gmail.com>
2022-10-16 18:38:03 -0700

    last commit

--- src/components/Picks.js
+++ src/components/Picks.js
@@ -11,41 +11,53 @@ constructor({resultDisplayer, Component, housePick, className, reset}){
     this.text = ''
 }
 state={
-    visible: false
+    visible: false,
+    win: ''
 }
 makeVisible=()=>{
+  let value;
     setTimeout(()=>{
         switch (this.className) {
             case 'rock':
               if (this.housePick['className'] === 'scissor') {
                 this.text = this.resultDisplayer('true')
+                value = 'user'
               }else if(this.housePick['className'] === 'paper'){
                 this.text = this.resultDisplayer('false')
+                value = 'comp'
               }else{
                  this.text = this.resultDisplayer('something else')
+                 value = ''
               }
               break;
             case 'paper':
               if (this.housePick['className'] === 'rock') {
                 this.text = this.resultDisplayer('true')
+                value = 'user'
               }else if(this.housePick['className'] === 'scissor'){
                 this.text = this.resultDisplayer('false')
+                value = 'comp'
               }else{
                 this.text = this.resultDisplayer('still a loss')
+                value = ''
               }
               break;
             case 'scissor':
               if (this.housePick['className'] === 'paper') {
                 this.text = this.resultDisplayer('true')
+                value = 'user'
                 }else if(this.housePick['className'] === 'rock'){
                   this.text = this.resultDisplayer('false')
+                  value = 'comp'
                 }else{
                   this.text = this.resultDisplayer('a loss to')
+                  value = ''
                 }
                 break;
           }
     this.setState({
-        visible: true
+        visible: true,
+        win: value
     })
     }, 1500)
 }
@@ -58,14 +70,14 @@ render(){
         <div className="pick">
             <div className="yourPick">
                 <p>YOU PICKED</p>
-                <div className={['obj', this.className, 'holder'].join(' ')}>
+                <div className={['obj', this.className,  this.state.win === 'user'? 'winner': '' ,'holder'].join(' ')}>
                     {this.Component}
                 </div>
             </div>
             { this.state.visible? <Result result={this.text} func={this.reset}/>: null}
             <div className="housePick">
                 <p>THE HOUSE PICK</p>
-                <div className={['obj', this.state.visible ? this.housePick['className'] : '', 'holder', this.state.visible? 'winner' :'default'].join(' ')}>
+                <div className={['obj', this.state.visible ? this.housePick['className'] : '', 'holder', this.state.win === 'comp'? 'winner' :'', this.state.visible ? '':'default' ].join(' ')}>
                     {this.state.visible ? this.housePick['obj']: null}
                 </div>
             </div>
